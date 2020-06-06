1938fe43d124cc86020192f7a08f425cfc08ca46
Vanille-N <neven.villani@gmail.com>
2020-06-06 16:32:13 +0200

    update benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,18 +1,39 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use core::time::Duration;
 
-use rask::parse::{lex, split, PROG};
+use rask::parse::{lex, split, source};
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("split", |b| b.iter(|| split(black_box(PROG))));
-    c.bench_function("split_and_lex", |b| {
-        b.iter(|| {
-            let sp = split(black_box(PROG)).ok().unwrap();
-            sp.into_iter()
-                .map(|s| lex(black_box(s)))
-                .collect::<Vec<_>>()
-        })
-    });
+fn criterion_split(c: &mut Criterion) {
+    let assets = ["sort", "set-construct", "word-count", "printer", "interprete", "unification", "timer", "sprintf", "matrix"];
+    let crit = std::mem::replace(&mut*c, Criterion::default())
+        .measurement_time(Duration::new(3, 0))
+        .warm_up_time(Duration::new(1, 0));
+    std::mem::replace(&mut*c, crit);
+
+    let mut group_split = c.benchmark_group("split");
+    for file in assets.iter() {
+        let prog = source(&("assets/".to_owned() + *file)).unwrap();
+        group_split.bench_with_input(*file, *file, |b, _| {
+            b.iter(|| split(black_box(&prog[..])))
+        });
+    }
+    group_split.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn criterion_lex(c: &mut Criterion) {
+    let assets = ["sort", "set-construct", "word-count", "printer", "interprete", "unification", "timer", "sprintf", "matrix"];
+
+    let mut group_lex = c.benchmark_group("lex");
+    for file in assets.iter() {
+        let prog = source(&("assets/".to_owned() + *file)).unwrap();
+        let sp = split(&prog[..]);
+        group_lex.bench_with_input(*file, *file, |b, _| {
+            b.iter(|| sp.iter().map(|s| black_box(s)).collect::<Vec<_>>())
+        });
+    }
+    group_lex.finish();
+}
+
+
+criterion_group!(benches, criterion_split, criterion_lex);
 criterion_main!(benches);
