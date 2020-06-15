b8b2df71893b18fd3956a00c2b0e158e1c73bae5
Vanille-N <neven.villani@gmail.com>
2020-06-15 09:44:09 +0200

    lib restructured

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,16 @@
+use std::fs::File;
+use std::io::Read;
+
 pub mod parse;
+
+pub fn source(fname: &str) -> Option<String> {
+    if let Ok(mut file) = File::open(fname.to_owned() + ".scm") {
+        let mut contents = String::new();
+        match file.read_to_string(&mut contents) {
+            Ok(_) => Some(contents),
+            Err(_) => None,
+        }
+    } else {
+        None
+    }
+}
