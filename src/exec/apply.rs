0014ec7e4308bfe52d32935e0b7619a6aaa5e3dd
Vanille-N <neven.villani@gmail.com>
2020-06-17 10:44:30 +0200

    easy cases of apply

--- src/exec/apply.rs
+++ src/exec/apply.rs
@@ -1,6 +1,14 @@
-use crate::exec::{EvalErr, Expr};
+use crate::exec::{EvalErr, Expr, eval, Envt};
 use std::rc::Rc;
 
-pub fn apply(lst: &Vec<Expr>) -> Result<Rc<Expr>, EvalErr> {
+pub fn apply(lst: &Vec<Rc<Expr>>, ctx: &mut Envt) -> Result<Rc<Expr>, EvalErr> {
+    if lst.len() == 0 {
+        return Ok(Rc::new(Expr::Nil));
+    }
+    match &*lst[0] {
+        Expr::Nil | Expr::Char(_) | Expr::Integer(_) | Expr::Float(_) | Expr::String(_) | Expr::Cons(_, _) | Expr::Quote(_) | Expr::Quasiquote(_) | Expr::Antiquote(_) => Err(EvalErr::CannotApply(lst[0].clone())),
+        _ => unreachable!(),
+    }
+}
     unimplemented!()
 }
