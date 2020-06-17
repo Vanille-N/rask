79335ebf707723b55687c1c805e6eb87a3d920f1
Vanille-N <neven.villani@gmail.com>
2020-06-17 10:44:58 +0200

    When first item of a list has to be eval'ed first

--- src/exec/apply.rs
+++ src/exec/apply.rs
@@ -7,6 +7,15 @@ pub fn apply(lst: &Vec<Rc<Expr>>, ctx: &mut Envt) -> Result<Rc<Expr>, EvalErr> {
     }
     match &*lst[0] {
         Expr::Nil | Expr::Char(_) | Expr::Integer(_) | Expr::Float(_) | Expr::String(_) | Expr::Cons(_, _) | Expr::Quote(_) | Expr::Quasiquote(_) | Expr::Antiquote(_) => Err(EvalErr::CannotApply(lst[0].clone())),
+        Expr::List(_) => {
+            let result = eval(lst[0].clone(), ctx)?;
+            match &*result {
+                Expr::Func(f) => f(&lst[1..]),
+                Expr::Lambda(f, _) => f(&lst[1..]),
+                Expr::Atom(a) => apply_atom(&a[..], &lst[1..], ctx),
+                _ => Err(EvalErr::CannotApply(result.clone())),
+            }
+        }
         _ => unreachable!(),
     }
 }
