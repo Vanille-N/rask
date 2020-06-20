9165967eadb4cadd09cc9392ace86788864eee75
Vanille-N <neven.villani@gmail.com>
2020-06-20 14:12:06 +0200

    equality for errors ignores actual contents (looks only at error type)

--- src/exec/util.rs
+++ src/exec/util.rs
@@ -1,5 +1,6 @@
 use crate::exec::Expr;
 use std::rc::Rc;
+use std::cmp;
 
 #[derive(Debug)]
 pub enum EvalErr {
@@ -11,3 +12,39 @@ pub enum EvalErr {
     TypeError,
     WrongArgList,
 }
+
+impl cmp::PartialEq for EvalErr {
+    fn eq(&self, other: &Self) -> bool {
+        macro_rules! identical {
+            ( $id:tt ) => {
+                match other {
+                    EvalErr::$id => true,
+                    _ => false,
+                }
+            };
+            ( $id:tt(_) ) => {
+                match other {
+                    EvalErr::$id(_) => true,
+                    _ => false,
+                }
+            };
+            ( $id:tt($contents:tt) ) => {
+                match other {
+                    EvalErr::$id(x) => x == $contents,
+                    _ => false,
+                }
+            };
+        }
+        match self {
+            EvalErr::UnknownIdent(_) => identical!(UnknownIdent(_)),
+            EvalErr::UselessAntiquote(_) => identical!(UselessAntiquote(_)),
+            EvalErr::CannotEval(_) => identical!(CannotEval(_)),
+            EvalErr::CannotApply(_) => identical!(CannotApply(_)),
+            EvalErr::ProperListRequired(_) => identical!(ProperListRequired(_)),
+            EvalErr::TypeError => identical!(TypeError),
+            EvalErr::WrongArgList => identical!(WrongArgList),
+        }
+    }
+}
+
+impl cmp::Eq for EvalErr {}
