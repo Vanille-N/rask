7786a28cb077cf834a9083dcc842225a0fde183b
Vanille-N <neven.villani@gmail.com>
2020-07-18 15:31:23 +0200

    alias functionality

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -3,6 +3,17 @@ use chainmap::ChainMap;
 
 mod operators;
 
+pub trait Alias {
+    fn alias(&mut self, s: &str, a: &str);
+}
+
+impl Alias for Envt {
+    fn alias(&mut self, s: &str, a: &str) {
+        let cpy = self.get(&a.to_string()).unwrap().clone();
+        self.insert(s.to_string(), cpy);
+    }
+}
+
 pub fn initialize_environment() -> Envt {
     let mut envt = ChainMap::new();
     operators::init(&mut envt);
