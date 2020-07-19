206c0144d6ce024bb4cdf595652ad3d5dac0491a
Vanille-N <neven.villani@gmail.com>
2020-07-19 08:20:34 +0200

    and + or for bools

--- src/init/stdbool.rs
+++ src/init/stdbool.rs
@@ -9,7 +9,7 @@ pub fn init(envt: &mut Envt) {
             if args.len() > 1 {
                 return Err(EvalErr::WrongArgList);
             }
-            match eval(args[0], ctx) {
+            match eval(args[0].clone(), ctx) {
                 Err(e) => Err(e),
                 Ok(val) => match &*val {
                     Expr::Bool(b) => Ok(Rc::new(Expr::Bool(!b))),
@@ -18,5 +18,39 @@ pub fn init(envt: &mut Envt) {
             }
         })))
     );
+    envt.insert(
+        String::from("__and"),
+        Rc::new(Expr::Func(Rc::new(|args, ctx| {
+            for x in args.iter() {
+                match eval(x.clone(), ctx) {
+                    Err(e) => return Err(e),
+                    Ok(val) => match &*val {
+                        Expr::Bool(false) => return Ok(Rc::new(Expr::Bool(false))),
+                        Expr::Bool(_) => (),
+                        _ => return Err(EvalErr::TypeError),
+                    }
+                }
+            }
+            Ok(Rc::new(Expr::Bool(true)))
+        })))
+    );
+    envt.insert(
+        String::from("__or"),
+        Rc::new(Expr::Func(Rc::new(|args, ctx| {
+            for x in args.iter() {
+                match eval(x.clone(), ctx) {
+                    Err(e) => return Err(e),
+                    Ok(val) => match &*val {
+                        Expr::Bool(true) => return Ok(Rc::new(Expr::Bool(true))),
+                        Expr::Bool(_) => (),
+                        _ => return Err(EvalErr::TypeError),
+                    }
+                }
+            }
+            Ok(Rc::new(Expr::Bool(false)))
+        })))
+    );
     envt.alias("not", "__not");
+    envt.alias("and", "__and");
+    envt.alias("or", "__or");
 }
