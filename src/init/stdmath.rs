98816fd5dcb4586d80f6faa6200af6f08c10d0ca
Vanille-N <neven.villani@gmail.com>
2020-08-02 06:14:11 +0200

    for a new function

--- src/init/stdmath.rs
+++ src/init/stdmath.rs
@@ -25,9 +25,51 @@ pub fn init(envt: &mut Envt) {
             }
         })))
     );
+    envt.insert(
+        String::from("__pow"),
+        Rc::new(Expr::Func(Rc::new(|args, ctx| {
+            if args.len() != 2 {
+                Err(EvalErr::WrongArgList)
+            } else {
+                match eval(args[0].clone(), ctx) {
+                    Ok(val) => {
+                        match &*val {
+                            Expr::Integer(n) => {
+                                match eval(args[1].clone(), ctx) {
+                                    Ok(val) => {
+                                        match &*val {
+                                            Expr::Integer(m) => Ok(Rc::new(Expr::Integer(n.powi(m)))),
+                                            Expr::Float(g) => Ok(Rc::new(Expr::Float((n as f64).pow(g)))),
+                                            _ => Err(EvalErr::TypeError),
+                                        }
+                                    }
+                                    Err(e) => Err(e),
+                                }
+                            }
+                            Expr::Float(f) => match eval(args[1].clone(), ctx) {
+                                Ok(val) => {
+                                    match &*val {
+                                        Expr::Integer(m) => Ok(Rc::new(Expr::Integer(n.powi(m)))),
+                                        Expr::Float(g) => Ok(Rc::new(Expr::Float(n.pow(g)))),
+                                        _ => Err(EvalErr::TypeError),
+                                    }
+                                }
+                                Err(e) => Err(e),
+                            }
+                            _ => {
+                                eprintln!("{:?}", args[0]);
+                                Err(EvalErr::TypeError)
+                            }
+                        }
+                    }
+                    Err(e) => Err(e),
+                }
+            }
+        })))
+    );
 
     envt.alias("exp", "__exp");
-    // envt.alias("pow", "__pow");
+    envt.alias("pow", "__pow");
     // envt.alias("math.pi", "__pi");
     // envt.alias("math.e", "__e");
     // envt.alias("cos", "__cos");
