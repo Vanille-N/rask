5a3f324f0787e0ca96496424aa7cd9f0ef0beaab
Vanille-N <neven.villani@gmail.com>
2020-08-04 08:21:30 +0200

    proper Drop

--- src/list.rs
+++ src/list.rs
@@ -37,6 +37,18 @@ impl<T> List<T> {
     }
 }
 
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
