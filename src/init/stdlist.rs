54e677e10b2dbd7e57464ad30dd5d3f77e436171
Vanille-N <neven.villani@gmail.com>
2020-08-07 08:33:56 +0200

    fn cons

--- src/init/stdlist.rs
+++ src/init/stdlist.rs
@@ -3,5 +3,20 @@ use crate::init::Alias;
 use std::rc::Rc;
 
 pub fn init(envt: &mut Envt) {
-    
+    envt.insert(
+        String::from("__cons"),
+        Rc::new(Expr::Func(Rc::new(|args, ctx| {
+            if args.len() != 2 {
+                return Err(EvalErr::WrongArgList);
+            }
+            let e = eval(args.head().unwrap().clone(), ctx)?;
+            let l = eval(args.tail().head().unwrap().clone(), ctx)?;
+            if let Expr::List(lst) = &*l {
+                Ok(Rc::new(Expr::List(Rc::new(lst.cons(e.clone())))))
+            } else {
+                Err(EvalErr::TypeError)
+            }
+        })))
+    );
+    envt.alias("cons", "__cons");
 }
