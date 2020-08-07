22f6a4aabea088df3a0d745b958b189760bb5efe
Vanille-N <neven.villani@gmail.com>
2020-08-07 08:47:08 +0200

    fn cdr

--- src/init/stdlist.rs
+++ src/init/stdlist.rs
@@ -35,6 +35,24 @@ pub fn init(envt: &mut Envt) {
             }
         })))
     );
+    envt.insert(
+        String::from("__cdr"),
+        Rc::new(Expr::Func(Rc::new(|args, ctx| {
+            if args.head().is_none() || args.tail().head().is_some() {
+                return Err(EvalErr::WrongArgList);
+            }
+            let l = eval(args.head().unwrap().clone(), ctx)?;
+            if let Expr::List(lst) = &*l {
+                match lst.head() {
+                    Some(h) => Ok(Expr::List(lst.tail())),
+                    None => Err(EvalErr::EmptyList),
+                }
+            } else {
+                Err(EvalErr::TypeError)
+            }
+        })))
+    );
     envt.alias("cons", "__cons");
     envt.alias("car", "__car");
+    envt.alias("cdr", "__cdr");
 }
