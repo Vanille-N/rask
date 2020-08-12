5d06b38a737074efd0316f5c9c5ff1c98859965d
Vanille-N <neven.villani@gmail.com>
2020-08-12 08:10:52 +0200

    eval function

--- src/init/stdmisc.rs
+++ src/init/stdmisc.rs
@@ -12,5 +12,16 @@ pub fn init(envt: &mut Envt) {
             Ok(args.head().unwrap().clone())
         }))),
     );
+    envt.insert(
+        String::from("__eval"),
+        Rc::new(Expr::Func(Rc::new(|args, ctx| {
+            if args.head().is_none() || args.tail().head().is_some() {
+                return Err(EvalErr::WrongArgList);
+            }
+            let result = eval(args.head().unwrap().clone(), ctx)?;
+            eval(result, ctx)
+        })))
+    );
     envt.alias("quote", "__quote");
+    envt.alias("eval", "__eval");
 }
